651bf85c19076688d57a4bd9b36b6f4857b751e7
Jaimin2203 <jaiminchahun6667@gmail.com>
2024-02-28 11:35:19 +0530

    New File Add .

---
+++ src/App.js
@@ -0,0 +1,155 @@
+import './App.css';
+import { EmployeData } from './EmployeData';
+import { useState, useEffect } from 'react';
+
+function App() {
+  const [data, setData] = useState([]);
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [age, setAge] = useState(0);
+  const [id, setId] = useState(0);
+  const [isUpadte, setIsUpadte] = useState(false);
+
+
+
+  useEffect(() => {
+    setData(EmployeData)
+  }, [])
+
+  const handleEdit = (id) => {
+    const dt = data.filter(item => item.id === id);
+    if (dt !== undefined) {
+      setIsUpadte(true)
+      setId(id)
+      setFirstName(dt[0].firstName)
+      setLastName(dt[0].lastName)
+      setAge(dt[0].age)
+
+    }
+  }
+
+
+  const handleDelete = (id) => {
+    if (id > 0) {
+      if (window.confirm("Are you sure to delete this item?")) {
+        const dt = data.filter(item => item.id !== id);
+        setData(dt);
+      }
+    }
+  }
+  const handleSave = (e) => {
+    let error = '';
+    if (firstName === '')
+      error += 'First name is required, ';
+
+    if (lastName === '')
+      error += 'Last name is required, ';
+
+    if (age <= 0)
+      error += 'Age name is required. ';
+    if (error === '') {
+
+
+      e.preventDefault();
+      const dt = [...data];
+      const newObject = {
+        id: EmployeData.length + 1,
+        firstName: firstName,
+        lastName: lastName,
+        age: age,
+      }
+      dt.push(newObject);
+      setData(dt);
+    } else {
+      alert(error);
+    }
+  }
+
+  const handleUpdate = () => {
+    const index = data.map((item) => {
+      return item.id
+    }).indexOf(id)
+
+    const dt = [...data];
+    dt[index].firstName = firstName;
+    dt[index].lastName = lastName;
+    dt[index].age = age;
+
+    setData(dt);
+    handleClear();
+  }
+
+  const handleClear = () => {
+    setId(0)
+    setFirstName('')
+    setLastName('')
+    setAge('')
+    setIsUpadte(false)
+  }
+  return (
+    <div className='App'>
+      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '10px', marginBottom: '10px' }}>
+        <div>
+          <label>First Name :
+            <input type='text' placeholder='Enter First name' onChange={(e) => setFirstName(e.target.value)} value={firstName} />
+          </label>
+        </div>
+        <div>
+          <label>Last Name :
+            <input type='text' placeholder='Enter Last name' onChange={(e) => setLastName(e.target.value)} value={lastName} />
+          </label>
+        </div>
+        <div>
+          <label>Age :
+            <input type='text' placeholder='Enter Age' onChange={(e) => setAge(e.target.value)} value={age} />
+          </label>
+        </div>
+        <div>
+          {
+            !isUpadte ?
+              <button className='btn btn-primary' onClick={(e) => handleSave(e)}>Save</button> :
+              <button className='btn btn-primary' onClick={() => handleUpdate()}>Update</button>
+          }
+
+          <button className='btn btn-danger' onClick={() => handleClear()}>Clear</button>
+        </div>
+      </div>
+
+
+      <table className='table table-hover'>
+        <thead>
+          <tr>
+            <td>Sr.No</td>
+            <td>Id</td>
+            <td>First Name</td>
+            <td>Last Name</td>
+            <td>Age</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            data.map((item, index) => {
+              return (
+                <tr key={index}>
+                  <td>{index + 1}</td>
+                  <td>{item.id}</td>
+                  <td>{item.firstName}</td>
+                  <td>{item.lastName}</td>
+                  <td>{item.age}</td>
+                  <td>
+                    <button className='btn btn-primary' onClick={() => handleEdit(item.id)}>Edit</button>
+                    <button className='btn btn-danger' onClick={() => handleDelete(item.id)}>Delete</button>
+                  </td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+
+      </table>
+    </div >
+  );
+}
+
+export default App;
